4436a1b410a600236f6b1ce16eaafb3879a083fe
Igor Cerqueira <igor.cerqueira@jussi.local>
2024-05-15 17:04:50 -0300

    initial setup, honojs, drizzle, neon, first components

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,33 @@
+import { Loader2 } from 'lucide-react'
+import { ClerkLoaded, ClerkLoading, SignUp } from '@clerk/nextjs'
+import Image from 'next/image'
+
+export default function Page() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">
+            Bem vindo de volta
+          </h1>
+          <p className="text-base text-[#7e8ca0]">
+            Faça o login ou crie sua conta para acessar o painel.
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignUp path="/sign-up" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+
+      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+        <Image src="logo.svg" width={100} height={100} alt="logo" />
+      </div>
+    </div>
+  )
+}
---
+++ components/navigation.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import Image from 'next/image'
+import { usePathname, useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { useMedia } from 'react-use'
+import { Menu } from 'lucide-react'
+
+import { Button } from './ui/button'
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
+import { NavButton } from '@/components/nav-button'
+
+const routes = [
+  { href: '/', label: 'Painel' },
+  { href: '/transacoes', label: 'Transações' },
+  { href: '/contas', label: 'Contas' },
+  { href: '/categorias', label: 'Categorias' },
+  { href: '/configuracoes', label: 'Configurações' },
+]
+
+export const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const router = useRouter()
+  const pathname = usePathname()
+  const isMobile = useMedia('(max-width: 1024px', false)
+
+  const onClick = (href: string) => {
+    router.push(href)
+    setIsOpen(false)
+  }
+
+  if (isMobile) {
+    return (
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger>
+          <Button
+            variant="outline"
+            size="sm"
+            className="font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition"
+          >
+            <Menu className="size-4" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="px-2">
+          <div className="flex absolute top-1.5">
+            <Image
+              src="/logo-menu-mobile.svg"
+              alt="logo"
+              width={28}
+              height={28}
+              className=""
+            />
+            <p className="font-semibold text-[#3b82f6] text-xl ml-2.5">
+              Finantial
+            </p>
+          </div>
+          <nav className="flex flex-col gap-2 pt-6">
+            {routes.map((route) => (
+              <Button
+                variant={route.href === pathname ? 'secondary' : 'ghost'}
+                onClick={() => onClick(route.href)}
+                className="w-full justify-start"
+              >
+                {route.label}
+              </Button>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    )
+  }
+
+  return (
+    <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
+      {routes.map((route) => (
+        <NavButton
+          key={route.href}
+          label={route.label}
+          href={route.href}
+          isActive={pathname === route.href}
+        />
+      ))}
+    </nav>
+  )
+}
