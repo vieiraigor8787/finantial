65989010d364b63490d83bb67b4cb060e847bb20
Igor Cerqueira <igor.cerqueira@jussi.local>
2024-05-16 15:34:54 -0300

    accounts get api

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,9 +1,19 @@
 import { Hono } from 'hono'
+import { HTTPException } from 'hono/http-exception'
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 
 import { db } from '@/db/drizzle'
 import { accounts } from '@/db/schema'
 
-const app = new Hono().get('/', async (c) => {
+const app = new Hono().get('/', clerkMiddleware(), async (c) => {
+  const auth = getAuth(c)
+
+  if (!auth?.userId) {
+    throw new HTTPException(401, {
+      res: c.json({ error: 'Unauthorized' }, 401),
+    })
+  }
+
   const data = await db
     .select({
       id: accounts.id,
