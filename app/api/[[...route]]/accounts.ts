c7c4c230154cd639fc7ec8036944a46eb15ee547
Igor Cerqueira <igor.cerqueira@jussi.local>
2024-05-17 12:02:03 -0300

    modifying config api

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,24 +1,15 @@
 import { Hono } from 'hono'
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 
 import { db } from '@/db/drizzle'
 import { accounts } from '@/db/schema'
-import { eq } from 'drizzle-orm'
 
-const app = new Hono().get('/', clerkMiddleware(), async (c) => {
-  const auth = getAuth(c)
-
-  if (!auth?.userId) {
-    return c.json({ error: 'Unauthorized' }, 401)
-  }
-
+const app = new Hono().get('/', async (c) => {
   const data = await db
     .select({
       id: accounts.id,
       name: accounts.name,
     })
     .from(accounts)
-    .where(eq(accounts.userId, auth.userId))
 
   return c.json({ data })
 })
