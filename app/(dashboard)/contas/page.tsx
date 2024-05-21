b13aa1d26e94b066c4cac0ecb13aa6296cdaab4d
Igor Cerqueira <igor.cerqueira@jussi.local>
2024-05-21 12:38:40 -0300

    using database infos into table, skeleton

--- app/(dashboard)/contas/page.tsx
+++ app/(dashboard)/contas/page.tsx
@@ -1,37 +1,37 @@
 'use client'
-import { Plus } from 'lucide-react'
+import { Loader2, Plus } from 'lucide-react'
 
 import { useNewAccount } from '@/features/accounts/hooks/use-new-accounts'
+import { useGetAccounts } from '@/features/accounts/api/use-get-accounts'
 
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { DataTable } from '@/components/data-table'
+import { Skeleton } from '@/components/ui/skeleton'
 
-import { Payment, columns } from './columns'
-
-const data: Payment[] = [
-  {
-    id: '728ed52f',
-    amount: 100,
-    status: 'pending',
-    email: 'm@example.com',
-  },
-  {
-    id: '728ed52f',
-    amount: 50,
-    status: 'processing',
-    email: 'e@example.com',
-  },
-  {
-    id: '728ed52f',
-    amount: 80,
-    status: 'failed',
-    email: 't@example.com',
-  },
-]
+import { columns } from './columns'
 
 const AccountsPage = () => {
   const newAccount = useNewAccount()
+  const accountsQuery = useGetAccounts()
+  const accounts = accountsQuery.data || []
+
+  if (accountsQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -46,7 +46,7 @@ const AccountsPage = () => {
         <CardContent>
           <DataTable
             columns={columns}
-            data={data}
+            data={accounts}
             filterKey="email"
             onDelete={() => {}}
           />
